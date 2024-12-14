0d3726b7466de198b21952fd31e0968e2331ff82
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-14 19:27:04 -0300

    refact: refactoring main, add env variables and change endpoint

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
-package Main
+package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/andreparelho/debit-authorizer/handler"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	http.HandleFunc("/approver", handler.ApproverHandler)
+const ENDPOINT_AUTHORIZER_DEBIT string = "/authorizer-debit"
 
-	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", nil)
+func main() {
+	var errorEnv error = godotenv.Load()
+	if errorEnv != nil {
+		log.Fatalf("Error to load file .env: %v", errorEnv)
+	}
+
+	var serverPort string = os.Getenv("SERVER_PORT")
+
+	http.HandleFunc(ENDPOINT_AUTHORIZER_DEBIT, handler.DebitAuthorizerHandler)
+
+	fmt.Println("Application is running on port 8080")
+	http.ListenAndServe(serverPort, nil)
 }
