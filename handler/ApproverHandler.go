0a35492413f57b61389ca6f29d55caa2c553adaf
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-14 18:49:20 -0300

    refactor: refactoring handler

--- handler/ApproverHandler.go
+++ handler/ApproverHandler.go
@@ -5,8 +5,9 @@ import (
 	"net/http"
 	"strconv"
 
-	requestHandler "github.com/andreparelho/credit-approver/model/handler/request"
-	service "github.com/andreparelho/credit-approver/service"
+	requestHandler "github.com/andreparelho/debit-authorizer/model/handler/request"
+	service "github.com/andreparelho/debit-authorizer/service"
+	httpUtil "github.com/andreparelho/debit-authorizer/util/httpUtil"
 )
 
 const POST_METHOD string = "POST"
@@ -14,10 +15,11 @@ const EMPTY string = ""
 const ZERO int64 = 0
 
 func ApproverHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	var httpUtil = httpUtil.ResponseJSONConstructor(responseWriter)
+
 	if request.Method != POST_METHOD {
 		var message []byte = []byte(`{"message": "Invalid method"}`)
-		responseWriter.Header().Set("Content-Type", "application/json")
-		responseWriter.WriteHeader(http.StatusMethodNotAllowed)
+		httpUtil.ResponseJSON(message, http.StatusMethodNotAllowed)
 		responseWriter.Write(message)
 		return
 	}
@@ -26,9 +28,7 @@ func ApproverHandler(responseWriter http.ResponseWriter, request *http.Request) 
 	var requestHandlerError error = json.NewDecoder(request.Body).Decode(&requestHandler)
 	if requestHandlerError != nil {
 		var message []byte = []byte(`{"message": "Error to decode json"}`)
-		responseWriter.Header().Set("Content-Type", "application/json")
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		responseWriter.Write(message)
+		httpUtil.ResponseJSON(message, http.StatusInternalServerError)
 		return
 	}
 
@@ -36,22 +36,24 @@ func ApproverHandler(responseWriter http.ResponseWriter, request *http.Request) 
 	if clientId == EMPTY {
 		var message []byte = []byte(`{"message": "Propertie clientId is empty"}`)
 		responseWriter.Header().Set("Content-Type", "application/json")
-		responseWriter.WriteHeader(http.StatusBadRequest)
-		responseWriter.Write(message)
+		httpUtil.ResponseJSON(message, http.StatusBadRequest)
 		return
 	}
 
 	var amountRequest int64 = requestHandler.Amount
 	if amount := strconv.Itoa(int(amountRequest)); amountRequest <= ZERO || amount == EMPTY {
 		var message []byte = []byte(`{"message": "Propertie amout is empty or less than zero"}`)
-		responseWriter.Header().Set("Content-Type", "application/json")
-		responseWriter.WriteHeader(http.StatusBadRequest)
-		responseWriter.Write(message)
+		httpUtil.ResponseJSON(message, http.StatusBadRequest)
 		return
 	}
 
 	defer request.Body.Close()
 
-	service := service.ServiceApproverImpl(responseWriter)
-	service.ApproverService(requestHandler)
+	message, errorService := service.ApproverService(requestHandler)
+	if errorService != nil {
+		httpUtil.ResponseJSON(message, http.StatusTooManyRequests)
+		return
+	}
+
+	httpUtil.ResponseJSON(message, http.StatusOK)
 }
