a39b044e41bd5d2bd075c6ff0b34840751e8a3ac
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-14 19:27:28 -0300

    refact: refactoring service

--- service/DebitAuthorizerService.go
+++ service/DebitAuthorizerService.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"sync"
 	"time"
 
@@ -9,37 +10,42 @@ import (
 	logger "github.com/andreparelho/debit-authorizer/util/logUtil"
 )
 
-const LAST_FIVE_MINUTES = 5 * time.Minute
+const LAST_FIVE_MINUTES = 1 * time.Minute
 
 var clientHistory = make(map[string]serviceDTO.Client)
 var mutex sync.Mutex
-var now time.Time = time.Now()
 
-func ApproverService(requestHandler requestHandler.RequestApproverHandler) ([]byte, error) {
+func DebitAuthorizerService(requestHandler requestHandler.RequestApproverHandler) ([]byte, error) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	client, isCreated := clientHistory[requestHandler.ClientId]
+	var now time.Time = time.Now()
+	var clientId = requestHandler.ClientId
+
+	client, isCreated := clientHistory[clientId]
 	if !isCreated {
 		clientHistory[requestHandler.ClientId] = serviceDTO.Client{
 			LastPayment: now,
 			TotalAmount: requestHandler.Amount,
 		}
-		logger.ServiceLoggerInfo(client, requestHandler.ClientId, "client created")
+		logger.ServiceLoggerInfo(client, clientId, "client created")
 	}
 
 	var totalAmount = client.TotalAmount + requestHandler.Amount
 	if totalAmount > 1000 && now.Sub(client.LastPayment) <= LAST_FIVE_MINUTES {
 		var message []byte = []byte(`{"message": "Sorry you have reached your debit limit"}`)
-		logger.ServiceLoggerInfo(client, requestHandler.ClientId, "Sorry you have reached your debit limit")
-		return message, nil
+		logger.ServiceLoggerInfo(client, clientId, "Sorry you have reached your debit limit")
+
+		var errorMessage error = errors.New("Sorry you have reached your debit limit")
+
+		return message, errorMessage
 	}
 
 	client.LastPayment = now
 	client.TotalAmount = totalAmount
-	clientHistory[requestHandler.ClientId] = client
+	clientHistory[clientId] = client
 
 	var message []byte = []byte(`{"message": "debit approved"}`)
-	logger.ServiceLoggerInfo(client, requestHandler.ClientId, "debit approved")
+	logger.ServiceLoggerInfo(client, clientId, "debit approved")
 	return message, nil
 }
