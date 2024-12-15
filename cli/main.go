63df57278d30eb53478817393d8dbed11df8867c
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-15 19:35:52 -0300

    refact: refactoring cli main

--- cli/main.go
+++ cli/main.go
@@ -2,33 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"time"
 
 	request "github.com/andreparelho/debit-authorizer/model/common"
 	"github.com/andreparelho/debit-authorizer/service"
 )
 
-func main() {
-	var endStatus = true
+const END_STRING string = "sim"
 
-	for endStatus != false {
+func main() {
+	var endStatus bool = true
+
+	for endStatus {
 		var clientId string
 		var amount float64
 		var endString string
+		var now time.Time = time.Now()
 
-		fmt.Print("Cliente ID: ")
+		fmt.Print("Client ID: ")
 		fmt.Scanln(&clientId)
 		fmt.Print("Amount: ")
 		fmt.Scanln(&amount)
 
 		var request = request.RequestAuthorizerDebit{
 			ClientId: clientId,
+			DateTime: now,
 			Amount:   amount,
 		}
 
-		service.DebitAuthorizerService(request)
+		_, errorService := service.DebitAuthorizerService(request)
+		if errorService != nil {
+			fmt.Println(errorService.Error())
+		} else {
+			fmt.Println("Debit approved")
+		}
 
 		fmt.Print("Deseja finalizar? SIM / NAO: ")
-		if endString == "SIM" {
+		fmt.Scanln(&endString)
+		if strings.Compare(strings.ToLower(endString), strings.ToLower(END_STRING)) == 0 {
 			endStatus = false
 		}
 	}
