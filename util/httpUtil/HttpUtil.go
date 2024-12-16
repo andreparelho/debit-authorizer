89b42267d2a1c72d45282bf8b7be4ebd9348cfcf
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-16 19:24:46 -0300

    feat: create new method

--- util/httpUtil/HttpUtil.go
+++ util/httpUtil/HttpUtil.go
@@ -1,6 +1,11 @@
 package util
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	model "github.com/andreparelho/debit-authorizer/model/service"
+)
 
 type HttpUtil struct {
 	ResponseWriter http.ResponseWriter
@@ -16,4 +21,11 @@ func (writer *HttpUtil) ResponseJSON(message []byte, statusCode int) {
 	writer.ResponseWriter.Header().Set("Content-Type", "application/json")
 	writer.ResponseWriter.WriteHeader(statusCode)
 	writer.ResponseWriter.Write(message)
+}
+
+func (writer *HttpUtil) ResponseJSONSuccess(historical model.Client, statusCode int) {
+	response, _ := json.Marshal(historical)
+	writer.ResponseWriter.Header().Set("Content-Type", "application/json")
+	writer.ResponseWriter.WriteHeader(statusCode)
+	writer.ResponseWriter.Write(response)
 }
