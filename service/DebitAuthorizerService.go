fd32ad3e04e1137975cad012f21b52a09d38947f
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-17 21:06:27 -0300

    refact: refactoring service

--- service/DebitAuthorizerService.go
+++ service/DebitAuthorizerService.go
@@ -33,13 +33,13 @@ func DebitAuthorizerService(request request.RequestAuthorizerDebit) (model.Clien
 
 	var clientId = request.ClientId
 	client, isCreated := transactionHitorical[clientId]
-	var totalAmount = client.TotalAmount + request.Amount
 
+	var totalAmount = client.TotalAmount + request.Amount
 	if totalAmount > MAX_TOTAL_AMOUNT && now.Sub(client.LastPayment) <= LAST_FIVE_MINUTES {
 		valueMessage = "sorry you have reached your debit limit"
 		var errorMessage error = errors.New(valueMessage)
 
-		logger.ServiceLoggerError(client.LastPayment, client.TotalAmount, clientId, valueMessage)
+		logger.ServiceLoggerError(clientId, request.Amount, totalAmount, errorMessage.Error())
 		return model.Client{}, errorMessage
 	}
 
@@ -47,28 +47,31 @@ func DebitAuthorizerService(request request.RequestAuthorizerDebit) (model.Clien
 		valueMessage = "sorry the amount sent is greater than the allowed limit"
 		var errorMessage error = errors.New(valueMessage)
 
-		logger.ServiceLoggerError(client.LastPayment, client.TotalAmount, client.ClientId, valueMessage)
+		logger.ServiceLoggerError(clientId, request.Amount, totalAmount, errorMessage.Error())
 		return model.Client{}, errorMessage
 	}
 
+	validateClient(isCreated, client, clientId, dateTime, request.Amount, totalAmount)
+
+	valueMessage = "debit authorized"
+	logger.ServiceLoggerInfo(clientId, client.LastPayment, totalAmount, valueMessage)
+
+	var response model.Client = repository.GetClientHitorical(clientId, transactionHitorical)
+	return response, nil
+}
+
+func validateClient(isCreated bool, client model.Client, clientId string, dateTime time.Time, amount float64, totalAmount float64) {
 	if !isCreated {
 		client = model.Client{
 			ClientId:    clientId,
 			LastPayment: dateTime,
-			TotalAmount: request.Amount,
+			TotalAmount: amount,
 			Historical:  []model.Historical{},
 		}
-
-		repository.CreateClientHistorical(transactionHitorical, client, dateTime, request.Amount)
-		logger.ServiceLoggerInfo(dateTime, request.Amount, clientId, "client created")
+		repository.CreateClientHistorical(transactionHitorical, client, dateTime, amount)
+		logger.ServiceLoggerInfo(clientId, client.LastPayment, totalAmount, "client created")
+	} else {
+		repository.UpdateClientHistorical(client, transactionHitorical, clientId, dateTime, totalAmount, amount)
+		logger.ServiceLoggerInfo(clientId, client.LastPayment, totalAmount, "client updated")
 	}
-
-	if isCreated {
-		repository.UpdateClientHistorical(client, transactionHitorical, clientId, dateTime, totalAmount, request.Amount)
-	}
-
-	valueMessage = "debit approved"
-	logger.ServiceLoggerInfo(client.LastPayment, client.TotalAmount, clientId, valueMessage)
-
-	return repository.GetClientHitorical(clientId, transactionHitorical), nil
 }
