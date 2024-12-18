08f5aa305efd7d74866dadb4f500b36de78d0c27
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-17 21:04:38 -0300

    refact: refactoring handler

--- api/handler/DebitAuthorizerHandler.go
+++ api/handler/DebitAuthorizerHandler.go
@@ -2,9 +2,10 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
-	requestHandler "github.com/andreparelho/debit-authorizer/model/common"
+	common "github.com/andreparelho/debit-authorizer/model/common"
 	service "github.com/andreparelho/debit-authorizer/service"
 	httpUtil "github.com/andreparelho/debit-authorizer/util/httpUtil"
 )
@@ -13,43 +14,51 @@ const POST_METHOD string = "POST"
 const EMPTY_VALUE string = ""
 const MINIMUM_AMOUNT_REQUEST float64 = 0.01
 
+var message []byte
+
 func DebitAuthorizerHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	defer request.Body.Close()
 	var httpUtil *httpUtil.HttpUtil = httpUtil.ResponseJSONConstructor(responseWriter)
 
 	if request.Method != POST_METHOD {
-		var message []byte = []byte(`{"message": "invalid method"}`)
+		message = []byte(`{"message": "invalid method"}`)
 		httpUtil.ResponseJSON(message, http.StatusMethodNotAllowed)
 		return
 	}
 
-	var requestHandler requestHandler.RequestAuthorizerDebit
+	var requestHandler common.RequestAuthorizerDebit
 	var requestHandlerError error = json.NewDecoder(request.Body).Decode(&requestHandler)
 	if requestHandlerError != nil {
-		var message []byte = []byte(`{"message": "error to decode json"}`)
+		message = []byte(`{"message": "error to decode json"}`)
 		httpUtil.ResponseJSON(message, http.StatusInternalServerError)
 		return
 	}
 
 	var clientId string = requestHandler.ClientId
 	if clientId == EMPTY_VALUE {
-		var message []byte = []byte(`{"message": "propertie clientId is empty"}`)
+		message = []byte(`{"message": "propertie clientId is empty"}`)
 		httpUtil.ResponseJSON(message, http.StatusBadRequest)
 		return
 	}
 
 	var amountRequest float64 = requestHandler.Amount
 	if amountRequest <= MINIMUM_AMOUNT_REQUEST {
-		var message []byte = []byte(`{"message": "propertie amount is empty or less than minimum"}`)
+		message = []byte(`{"message": "propertie amount is empty or less than minimum"}`)
 		httpUtil.ResponseJSON(message, http.StatusBadRequest)
 		return
 	}
 
-	response, errorService := service.DebitAuthorizerService(requestHandler)
+	responseService, errorService := service.DebitAuthorizerService(requestHandler)
 	if errorService != nil {
-		var message []byte = []byte(`{"message": "sorry you have reached your debit limit"}`)
+		var errorMessage string = fmt.Sprintf(`{"message": "%s"}`, errorService.Error())
+		message = []byte(errorMessage)
 		httpUtil.ResponseJSON(message, http.StatusTooManyRequests)
 		return
+	}
+
+	var response common.ResponseAuthorizerDebit = common.ResponseAuthorizerDebit{
+		Message:          "debit authorized",
+		ClientHistorical: responseService,
 	}
 
 	httpUtil.ResponseJSONSuccess(response, http.StatusOK)
