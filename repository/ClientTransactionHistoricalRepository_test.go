c588cb40b510f6b6c57fcfd12bc899f7e04c5019
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-18 22:38:55 -0300

    test: create new test and increment update test

--- repository/ClientTransactionHistoricalRepository_test.go
+++ repository/ClientTransactionHistoricalRepository_test.go
@@ -41,10 +41,9 @@ func TestClientTransactionHistoricalRepository(test *testing.T) {
 		}
 	})
 
-	test.Run("Deve validar a atualzacao do historico quando chamar a funcao CreateClientHistorical", func(test *testing.T) {
+	test.Run("Deve validar a atualzacao do historico quando chamar a funcao UpdateClientHistorical", func(test *testing.T) {
 		var transactionHistorical = make(map[string]model.Client)
 		var dateTime time.Time = time.Now()
-		var amount float64 = 500
 
 		var client model.Client = model.Client{
 			ClientId:    "1",
@@ -56,6 +55,16 @@ func TestClientTransactionHistoricalRepository(test *testing.T) {
 		repository.CreateClientHistorical(transactionHistorical, client, dateTime, 100)
 		assert.True(test, client.TotalAmount == 100)
 
+		client = transactionHistorical[client.ClientId]
+
+		client = model.Client{
+			ClientId:    client.ClientId,
+			LastPayment: client.LastPayment,
+			TotalAmount: client.TotalAmount,
+			Historical:  client.Historical,
+		}
+
+		var amount float64 = 500
 		var totalAmount float64 = client.TotalAmount + amount
 		repository.UpdateClientHistorical(client, transactionHistorical, client.ClientId, dateTime, totalAmount, amount)
 
@@ -63,5 +72,38 @@ func TestClientTransactionHistoricalRepository(test *testing.T) {
 
 		assert.True(test, client.ClientId == "1")
 		assert.True(test, client.TotalAmount == 600)
+		assert.True(test, len(client.Historical) == 2)
+
+		historical := client.Historical
+
+		assert.True(test, historical[0].Amount == 100)
+		assert.True(test, historical[1].Amount == 500)
+	})
+
+	test.Run("Deve retornar um client valido quando chamar GetClientHitorical", func(test *testing.T) {
+		var transactionHistorical = make(map[string]model.Client)
+		var dateTime time.Time = time.Now()
+		var amount float64 = 100
+
+		var client model.Client = model.Client{
+			ClientId:    "Andre",
+			LastPayment: dateTime,
+			TotalAmount: amount,
+			Historical:  []model.Historical{},
+		}
+
+		assert.Empty(test, client.Historical)
+
+		repository.CreateClientHistorical(transactionHistorical, client, dateTime, amount)
+
+		client = transactionHistorical[client.ClientId]
+
+		var response model.Client = repository.GetClientHitorical(client.ClientId, transactionHistorical)
+
+		assert.NotEmpty(test, response)
+		assert.Equal(test, response, client)
+		assert.Equal(test, response.ClientId, client.ClientId)
+		assert.Equal(test, response.Historical, client.Historical)
+		assert.Equal(test, response.TotalAmount, client.TotalAmount)
 	})
 }
