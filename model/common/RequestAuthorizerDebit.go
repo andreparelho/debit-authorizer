51252e774d9315ede9d511e7fb7782d563319436
Andre Parelho <parelhoneves.andre@gmail.com>
2025-04-21 19:28:38 -0300

    refact: deleted files

--- model/common/RequestAuthorizerDebit.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "time"
-
-type RequestAuthorizerDebit struct {
-	ClientId string    `json:"clientId"`
-	DateTime time.Time `json:"dateTime"`
-	Amount   float64   `json:"amount"`
-}
--- model/common/ResponseAuthorizerDebit.go
+++
@@ -1,8 +0,0 @@
-package model
-
-import model "github.com/andreparelho/debit-authorizer/model/service"
-
-type ResponseAuthorizerDebit struct {
-	Message          string       `json:"message"`
-	ClientHistorical model.Client `json:"transactions"`
-}
